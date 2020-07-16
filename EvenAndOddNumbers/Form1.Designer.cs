b675b529fbfbff9ec0570bd73d92f4ba5c92e4ff
cdup <cdup@MSI>
2020-07-17 11:51:11 +1200

    Add project files.

---
+++ EvenAndOddNumbers/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace EvenAndOddNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DisplayList
+            // 
+            this.DisplayList.FormattingEnabled = true;
+            this.DisplayList.ItemHeight = 16;
+            this.DisplayList.Location = new System.Drawing.Point(225, 25);
+            this.DisplayList.Name = "DisplayList";
+            this.DisplayList.Size = new System.Drawing.Size(167, 324);
+            this.DisplayList.TabIndex = 0;
+            //this.DisplayList.SelectedIndexChanged += new System.EventHandler(this.DisplayList_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DisplayList);
+            this.Name = "Form1";
+            this.Text = "Even and & Odd numbers";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox DisplayList;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EvenAndOddNumbers/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvenAndOddNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // list of numbers
+
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+
+                result = numbers[i] % 2; // working out if its odd or even
+               
+                if (result == 0) // check the result to find out if its odd or even
+                { 
+                    DisplayList.Items.Add("Even" + " " + numbers[i]);
+                }
+                else {
+                    DisplayList.Items.Add("Odd" + " " + numbers[i]);
+                }
+            }
+        }
+    }
+}
